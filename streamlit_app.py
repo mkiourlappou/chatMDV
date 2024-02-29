09fa00fd05aa41a489b53eee5edd9ef34edfeb56
Maria Kiourlappou <45387897+mkiourlappou@users.noreply.github.com>
2024-02-29 15:15:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,214 @@
-import nest_asyncio
-from llama_index.readers.github_readers.github_repository_reader import (
-    GithubRepositoryReader,
-)
-import os
+# LangChain
+from langchain.llms import VertexAI
+from langchain.embeddings import VertexAIEmbeddings
+
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema.document import Document
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import Language
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+import time
+from typing import List
+from pydantic import BaseModel
+
+# Vertex AI
+from google.cloud import aiplatform
+import vertexai
+from vertexai.language_models import CodeGenerationModel
+
+# Streamlit
 import streamlit_app as st
-from llama_index.llms import OpenAI
-import openai
-from llama_index import VectorStoreIndex, ServiceContext
 
-nest_asyncio.apply()
-openai_key = os.getenv('OPENAI_API_KEY')
-
-openai.api_key = st.secrets.openai_key
-st.header("Chat with the medical pipeline assistant 💬 📚")
-
-if "messages" not in st.session_state.keys(): 
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about SpOOx!"}
-    ]
-
-github_token = os.getenv('GITHUB_TOKEN')
-owner = "mostafaosama999"
-branch = "main"
-
-@st.cache_resource(show_spinner=False)
-def load_index():
-    with st.spinner(text="Loading and indexing the spoox pipeline and other pipelines - hang tight! This should take 1-2 minutes."):
-        # 1. Embeddings / OpenAI Usage
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4-0125-preview", temperature=0.5, 
-            system_prompt="You are an expert on medical pipelines that utilitse WDL, nextflow and ruffus. Keep your answers technical and based on facts – do not hallucinate features."))
-
-        # 2. Data loader
-        docs_SpOOx = GithubRepositoryReader(
-                        github_token=github_token,
-                        owner=owner,
-                        repo="SpOOx",
-                        use_parser=False,
-                        verbose=True,
-                    ).load_data(branch=branch)
-        # 3. Index
-        index = VectorStoreIndex.from_documents(docs_SpOOx,show_progress=True, service_context=service_context)
-        return index
-
-index = load_index()
-
-# 4. ChatEngine
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+# Other
+import os
+import google.auth
+from pprint import pprint
+import requests, time
 
 
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            # 5. Chat response generator
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) 
-            
+### The following needs fixing with the Secrets thing
+import google.auth
+
+# Retrieve the JSON key file path from Streamlit Secrets
+key_path = st.secrets["key_file_path"]
+
+# Set the environment variable to point to the key file
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+
+# Authenticate using the key file
+# credentials, project_id = google.auth.default()
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+
+vertexai.init(project=st.secrets["file_id"])
+
+GITHUB_TOKEN = st.secrets["github_token"]  # @param {type:"string"}
+GITHUB_REPO = "mkiourlappou/chatMDV"  # @param {type:"string"}
+
+
+### Streamlit components
+st.title("🦜🔗 Quickstart App")
+
+# openai_api_key = st.sidebar.text_input("OpenAI API Key")
+
+
+### LLM
+code_llm = VertexAI(
+    model_name="code-bison@002",
+    max_output_tokens=2048,
+    temperature=0.1,
+    verbose=False,
+)
+
+
+# Crawls a GitHub repository and returns a list of all ipynb files in the repository
+def crawl_github_repo(url, is_sub_dir, access_token=f"{GITHUB_TOKEN}"):
+
+    ignore_list = ["__init__.py"]
+
+    if not is_sub_dir:
+        api_url = f"https://api.github.com/repos/{url}/contents"
+    else:
+        api_url = url
+
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    response = requests.get(api_url, headers=headers)
+    response.raise_for_status()  # Check for any request errors
+
+    files = []
+
+    contents = response.json()
+
+    for item in contents:
+        if (
+            item["type"] == "file"
+            and item["name"] not in ignore_list
+            and (item["name"].endswith(".py") or item["name"].endswith(".ipynb"))
+        ):
+            files.append(item["html_url"])
+        elif item["type"] == "dir" and not item["name"].startswith("."):
+            sub_files = crawl_github_repo(item["url"], True)
+            time.sleep(0.1)
+            files.extend(sub_files)
+
+    return files
+
+
+code_files_urls = crawl_github_repo(GITHUB_REPO, False, GITHUB_TOKEN)
+
+# Write list to a file so you do not have to download each time
+with open(
+    "/Users/maria/Documents/wellcome_human_gen_project/ChatBioinformatics/code_files_urls_chatBio.txt",
+    "w",
+) as f:
+    for item in code_files_urls:
+        f.write(item + "\n")
+
+
+# Extracts the python code from an .py file from github
+def extract_python_code_from_py(github_url):
+    raw_url = github_url.replace("github.com", "raw.githubusercontent.com").replace(
+        "/blob/", "/"
+    )
+    response = requests.get(raw_url)
+    response.raise_for_status()  # Check for any request errors
+
+    python_code = response.text
+
+    return python_code
+
+
+with open(
+    "/Users/maria/Documents/wellcome_human_gen_project/ChatBioinformatics/code_files_urls_chatBio.txt"
+) as f:
+    code_files_urls = f.read().splitlines()
+
+code_strings = []
+
+for i in range(0, len(code_files_urls)):
+    if code_files_urls[i].endswith(".py"):
+        content = extract_python_code_from_py(code_files_urls[i])
+        doc = Document(
+            page_content=content, metadata={"url": code_files_urls[i], "file_index": i}
+        )
+        code_strings.append(doc)
+
+# Chunk code strings
+text_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=1500, chunk_overlap=150
+)
+texts = text_splitter.split_documents(code_strings)
+
+EMBEDDING_QPM = 100
+EMBEDDING_NUM_BATCH = 5
+embeddings = VertexAIEmbeddings(
+    requests_per_minute=EMBEDDING_QPM,
+    num_instances_per_batch=EMBEDDING_NUM_BATCH,
+    model_name="textembedding-gecko@latest",
+)
+
+# Create Index from embedded code chunks
+db = FAISS.from_documents(texts, embeddings)
+
+# Init your retriever.
+retriever = db.as_retriever(
+    search_type="similarity",  # Also test "similarity", "mmr"
+    search_kwargs={"k": 5},
+)
+
+# RAG template
+prompt_RAG = """
+    You are a proficient python developer. Respond with the syntactically correct python code to the {question} below. Make sure you follow these rules:
+    1. Base the structure of the python code on the structure of files in the {context}
+    2. Generate the contents of a python file that serves an MDV project
+    
+    Context:
+    {context}
+
+    """
+
+prompt_RAG_template = PromptTemplate(
+    template=prompt_RAG, input_variables=["context", "question"]
+)
+
+qa_chain = RetrievalQA.from_llm(
+    llm=code_llm,
+    prompt=prompt_RAG_template,
+    retriever=retriever,
+    return_source_documents=True,
+)
+
+# user_question = "Heatmap of the isoform extension, the isoform mean length, the isoform min length and the isoform max length VS the Drosophila label status."
+
+# results = qa_chain({"query": user_question})
+
+
+def generate_response(input_text):
+    qa_chain = RetrievalQA.from_llm(
+        llm=code_llm,
+        prompt=prompt_RAG_template,
+        retriever=retriever,
+        return_source_documents=True,
+    )
+    st.info(qa_chain(input_text))
+
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        "What are the three key pieces of advice for learning how to code?",
+    )
+    submitted = st.form_submit_button("Submit")
+    # if not openai_api_key.startswith("sk-"):
+    #    st.warning("Please enter your OpenAI API key!", icon="⚠")
+    # if submitted and openai_api_key.startswith("sk-"):
+    generate_response(text)
