e458205ac75675ee6b511b918f7e6defdd9bf0aa
Maria Kiourlappou <45387897+mkiourlappou@users.noreply.github.com>
2024-03-01 09:03:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcs_connections"]
 
-client = documentai.DocumentProcessorServiceClient(credentials=credentials)
+documentai.DocumentProcessorServiceClient(credentials=credentials)
 
 
 
