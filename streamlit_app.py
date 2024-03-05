e95b35ffc25f9cb3f73d1b90a55f9426fdadf35f
Maria Kiourlappou <45387897+mkiourlappou@users.noreply.github.com>
2024-03-05 10:41:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ from pprint import pprint
 import requests, time
 
 import google.auth
+from google.oauth2 import service_account
+
 
 credentials = service_account.Credentials.from_service_account_info(st.secrets["credentials"])
 
